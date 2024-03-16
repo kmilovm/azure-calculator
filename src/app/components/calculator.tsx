4750f2074879ccafd2bf857e0a855a5713db3175
¨kmilovm¨ <kmilovm@hotmail.com>
2024-03-15 21:29:10 -0500

    little tweeks to ease use, adding production vars

--- src/app/components/calculator.tsx
+++ src/app/components/calculator.tsx
@@ -10,35 +10,26 @@ import { useEffect, useState } from "react";
 import * as signalR from "@microsoft/signalr";
 import { SignalRConnectionInfo } from "../models/signalRConnectionInfo";
 import { SignalRMessage } from "../models/signalRMessage";
+import CardHeader from "@mui/material/CardHeader";
+import IconButton from "@mui/material/IconButton";
+import Divider from "@mui/material/Divider";
 
 const CalculatorForm: React.FC = () => {
   const [number1, setNumber1] = useState<number>(0);
   const [number2, setNumber2] = useState<number>(0);
   const [operation, setOperation] = useState<string>("add");
   const [result, setResult] = useState<string | null>(null);
-  const [signalRConnectionInfo, setSignalRConnectionInfo] = useState<SignalRConnectionInfo>();
+  const [signalRConnectionInfo, setSignalRConnectionInfo] =  useState<SignalRConnectionInfo>();
 
   useEffect(() => {
     if (!signalRConnectionInfo) {
       const getSignalRConnectionInfo = async () => {
-        const response = await fetch(process.env.NEXT_PUBLIC_FUNCTION_NEGOCIATE, {
-          method: "POST",
-          headers: {
-            "x-ms-client-principal-id": "kmi",
-          }
-        });
-    
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        } else {
-          response.json().then(result => {
-            console.log(result)
-            setSignalRConnectionInfo(result)
-          });
-        }
+        const url = process.env.NEXT_PUBLIC_FUNCTION_NEGOCIATE!;
+        await getSignalConnectionInfo(url, setSignalRConnectionInfo);
       };
       getSignalRConnectionInfo();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -61,25 +52,42 @@ const CalculatorForm: React.FC = () => {
   }, [signalRConnectionInfo]);
 
   const handleCalculate = async () => {
-    setResult('Sending operation to server, please wait!');
-    const signalRMessage : SignalRMessage = {
-      userId:"calculator",
-      num1:number1,
-      num2:number2,
-      operation:operation
-    }    
-    const response = await fetch(process.env.NEXT_PUBLIC_FUNCTION_API, {
+    setResult("Sending operation to server, please wait!");
+    const signalRMessage: SignalRMessage = {
+      userId: "calculator",
+      num1: number1,
+      num2: number2,
+      operation: operation,
+    };
+    const url = process.env.NEXT_PUBLIC_FUNCTION_API!;
+    await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(signalRMessage),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }    
+    })
+      .then((res) => {})
+      .catch((error) => {
+        throw new Error(`HTTP error! status: ${error}`);
+      });
   };
+
+  async function getSignalConnectionInfo(url: string, setSignalRConnectionInfo: React.Dispatch<React.SetStateAction<SignalRConnectionInfo | undefined>>) {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "x-ms-client-principal-id": "Calculate",
+      },
+    }).then((res) => {
+      res.json().then((result) => {
+        setSignalRConnectionInfo(result);
+      });
+    }).catch((error) => {
+      console.log("error", error);
+      throw new Error(`HTTP error! status: ${error}`);
+    });
+  }
 
   return (
     <Box
@@ -89,47 +97,71 @@ const CalculatorForm: React.FC = () => {
       style={{ minHeight: "100vh" }}
     >
       <Card sx={{ minWidth: 275 }}>
+        <CardHeader
+          action={<IconButton aria-label="settings"></IconButton>}
+          title="Welcome to Azure Calculator"
+          subheader="Please use the wheel or arrows to select the numbers and the operation and press calculate button"
+        />
+        <Divider />
         <CardContent>
-          <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-            Welcome to Azure Calculator
-          </Typography>
-          <input
-            type="number"
-            title="number1"
-            placeholder="Enter number 1"
-            className="w-full p-2 mb-2 border rounded"
-            value={number1}
-            onChange={(e) => setNumber1(Number(e.target.value))}
-          />
-          <select
-            title="operation"
-            className="w-full p-2 mb-2 border rounded"
-            value={operation}
-            onChange={(e) => setOperation(e.target.value)}
+          <Box
+            sx={{
+              display: "flex",
+              m: 1,
+              p: 1,
+              bgcolor: (theme) =>
+                theme.palette.mode === "dark" ? "#101010" : "#fff",
+              color: (theme) =>
+                theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+              border: "1px solid",
+              borderColor: (theme) =>
+                theme.palette.mode === "dark" ? "grey.800" : "grey.300",
+              borderRadius: 2,
+              fontSize: "0.875rem",
+              fontWeight: "700",
+            }}
           >
-            <option value="add">Add</option>
-            <option value="substract">Subtract</option>
-            <option value="multiply">Multiply</option>
-            <option value="divide">Divide</option>
-          </select>
-          <input
-            type="number"
-            title="number2"
-            placeholder="Enter number 2"
-            className="w-full p-2 mb-2 border rounded"
-            value={number2}
-            onChange={(e) => setNumber2(Number(e.target.value))}
-          />
+            <input
+              type="number"
+              title="number1"
+              placeholder="Enter number 1"
+              className="w-full p-2 mb-2 border rounded text-center"
+              value={number1}
+              onKeyPress={(event) => { event.preventDefault(); }} onPaste={(event) => { event.preventDefault(); }} 
+              onChange={(e) => setNumber1(Number(e.target.value))}
+            />
+            <select
+              title="operation"
+              className="w-full p-2 mb-2 border rounded"
+              value={operation}
+              onChange={(e) => setOperation(e.target.value)}
+            >
+              <option value="add">Add</option>
+              <option value="subtract">Subtract</option>
+              <option value="multiply">Multiply</option>
+              <option value="divide">Divide</option>
+            </select>
+            <input
+              type="number"
+              title="number2"
+              placeholder="Enter number 2"
+              className="w-full p-2 mb-2 border rounded text-center"
+              value={number2}
+              onKeyPress={(event) => { event.preventDefault(); }} onPaste={(event) => { event.preventDefault(); }} 
+              onChange={(e) => setNumber2(Number(e.target.value))}
+            />
+          </Box>
         </CardContent>
         <CardActions>
-          <Button size="small" onClick={handleCalculate}>
-            Calculate
-          </Button>
+          <Box display="flex" justifyContent="left" alignItems="left">
+            <Button size="small" onClick={handleCalculate}>
+              Calculate
+            </Button>
+          </Box>
         </CardActions>
         <Box display="flex" justifyContent="center" alignItems="center">
           <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-            Resultant:{" "}
-            <b>{result != null ? result : "Not computed yet"}</b>
+            Resultant: <b>{result ?? "Not computed yet"}</b>
           </Typography>
         </Box>
       </Card>
@@ -138,3 +170,5 @@ const CalculatorForm: React.FC = () => {
 };
 
 export default CalculatorForm;
+
+
